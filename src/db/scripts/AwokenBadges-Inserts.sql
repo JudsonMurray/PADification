fc623cb3c8f0e1793554a3a8858675570acad481
Elie Godbout <elie.godbout@mckenzie.edu>
2017-06-27 11:58:52 -0300

    June 27th 2017 - Updated the descriptions for some of the Awoken badges from "Fix it " to the official description.

--- src/db/scripts/AwokenBadges-Inserts.sql
+++ src/db/scripts/AwokenBadges-Inserts.sql
@@ -13,8 +13,8 @@ Values
 ('Skillboost', 'Team�s skills charged by 1 turn'),
 ('Leaders Bind immune', 'Team Leaders become Bind immune'),
 ('Skill Bind resists +50%', 'Increases Skill Bind Resist by 50%'),
-('Team HP +15% / Team Cost -300', 'Fix it!'), 
-('Move Time +2 seconds / Team Cost -400', 'Fix it!'),
-('Team RCV +35% / Team Cost -300', 'Fix it!'),
-('No Skyfall Combos', 'Fix it!'),
-('Team ATK +15% / Team Cost -300', 'Fix it!')
+('Team HP +15% / Team Cost -300', 'Increases Team HP by 15%, but decreases Max Team Cost by 300'), 
+('Move Time +2 seconds / Team Cost -400', 'Extends Orb move time by 2 seconds, but decreases Max Team Cost by 400'),
+('Team RCV +35% / Team Cost -300', 'Increases Team RCV by 35%, but decreases Max Team Cost by 300'),
+('No Skyfall Combos', 'When erasing Orbs, new ones won’t appear until the turn is over'),
+('Team ATK +15% / Team Cost -300', 'Increases Team ATK by 15%, but decreases Max Team Cost by 300');
