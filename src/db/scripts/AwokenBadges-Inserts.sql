65a7f75b6161455c87775259849be841591152a4
Elie Godbout <elie.godbout@mckenzie.edu>
2017-06-27 12:03:27 -0300

    June 27th 2017 - Fixed an issue with the description's formating

--- src/db/scripts/AwokenBadges-Inserts.sql
+++ src/db/scripts/AwokenBadges-Inserts.sql
@@ -10,11 +10,11 @@ Values
 ('Team RCV +25%', 'Increases Team RCV by 25%'),
 ('Team HP +5%', 'Increases Team HP by 5%'),
 ('Team ATK +5%', 'Increases Team ATK by 5%'),
-('Skillboost', 'Team�s skills charged by 1 turn'),
+('Skillboost', 'Team''s skills charged by 1 turn'),
 ('Leaders Bind immune', 'Team Leaders become Bind immune'),
 ('Skill Bind resists +50%', 'Increases Skill Bind Resist by 50%'),
 ('Team HP +15% / Team Cost -300', 'Increases Team HP by 15%, but decreases Max Team Cost by 300'), 
 ('Move Time +2 seconds / Team Cost -400', 'Extends Orb move time by 2 seconds, but decreases Max Team Cost by 400'),
 ('Team RCV +35% / Team Cost -300', 'Increases Team RCV by 35%, but decreases Max Team Cost by 300'),
-('No Skyfall Combos', 'When erasing Orbs, new ones won’t appear until the turn is over'),
+('No Skyfall Combos', 'When erasing Orbs, new ones won''t appear until the turn is over'),
 ('Team ATK +15% / Team Cost -300', 'Increases Team ATK by 15%, but decreases Max Team Cost by 300');
