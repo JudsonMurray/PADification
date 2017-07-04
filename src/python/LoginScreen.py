6eed5071ac5df9fcf1d57d9301f1ba6b7191e788
Zach Blue <zachary.blue@mckenzie.edu>
2017-07-04 08:48:18 -0300

    Updated Account Options and Login code and UI
    Added in code and UI changes from test integration folder

--- src/python/LoginScreen.py
+++ src/python/LoginScreen.py
@@ -26,6 +26,11 @@ class LoginScreen:
         item = self.builder.get_object('TitleCanvas').create_image(10,0,image = self.image1,anchor = NW,tag="Title")
         self.builder.connect_callbacks(self)
 
+        self.obj1 = self.builder.get_object('Username',master)
+        self.obj2 = self.builder.get_object('Password',master)
+        self.isUsername = self.obj1.get()
+        self.isPassword = self.obj2.get()
+
     #Activates next screen when button is clicked
     def onCreateAccountClick(self, event):
         """Occurs When Create Account Button Is Clicked"""
@@ -38,3 +43,23 @@ class LoginScreen:
             self.master.showHomeScreen()
         else:
             mb.showwarning('Login Error', 'Username and Password Do not exist!')
+
+    def onUsernameFocusIn(self,event):
+        """Clears Username Entry Field"""
+        if self.obj1.get() == self.isUsername:
+            self.obj1.delete(0,END)
+
+    def onUsernameFocusOut(self,event):
+        """Clears Username Entry Field"""
+        if self.obj1.get() == "":
+            self.obj1.insert(0,"Username")
+
+    def onPasswordFocusIn(self,event):
+        """Clears Password Entry Field"""
+        if self.obj2.get() == self.isPassword:
+            self.obj2.delete(0,END)
+
+    def onPasswordFocusOut(self,event):
+        """Clears Password Entry Field"""
+        if self.obj2.get() == "":
+            self.obj2.insert(0,"Password")
