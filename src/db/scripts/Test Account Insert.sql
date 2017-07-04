40ca583cee1efbc200c59c1b3d98f1d927d9d4e4
Ryan Breau <ryanbreau2@gmail.com>
2017-07-04 14:12:42 -0300

    Created Test Account Insert for Testing

---
+++ src/db/scripts/Test Account Insert.sql
@@ -0,0 +1,21 @@
+use PADification
+
+insert into Player(Username, Password, PlayerID) Values 
+('Test', 'Test', 000000000)
+
+insert into MonsterInstance(Username, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
+('Test', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('Test', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('Test', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('Test', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('Test', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('Test', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('Test',2592,5000000,51,68,52,9,Null,1,Null),
+('Test',3284,4537584,99,35,84,1,Null,1,Null),
+('Test',3581,5000000,3,53,3,8,Null,1,Null),
+('Test',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('Test',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('Test',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('Test', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('Test', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL),
+('Test', 2495, 4000000, 99, 99, 99, 7, 40025, 2, null);
