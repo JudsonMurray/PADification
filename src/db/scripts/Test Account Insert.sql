a2baf504e8bc881bce0471f1ee6814a788b5755d
Elie Godbout <elie.godbout@mckenzie.edu>
2017-07-17 14:19:11 -0300

    July 17th 2017 - Added more users (11 total), updated inserts (60 for padmin, 14 padmin2 to padmin4) and added inserts for teams (2 for padmin, 1 for padmin2 to padmin4)

--- src/db/scripts/Test Account Insert.sql
+++ src/db/scripts/Test Account Insert.sql
@@ -4,8 +4,15 @@ insert into Player(Email,Username, Password, PlayerID) Values
 ('PADmin', 'Test', 'Test', 000000000),
 ('PADmin2', 'Test2', 'Test2', 000000001),
 ('PADmin3', 'Test3', 'Test3', 000000002),
-('PADification@gmail.com', 'Test', 'Test', 1);
+('PADification@gmail.com', 'Test', 'Test', 1),
+('PADmin4', 'Test4', 'Test4', 000000003),
+('PADmin5', 'Test5', 'Test5', 000000004),
+('PADmin6', 'Test6', 'Test6', 000000005),
+('PADmin7', 'Test7', 'Test7', 000000006),
+('PADmin8', 'Test8', 'Test8', 000000007),
+('PADmin9', 'Test9', 'Test9', 000000008);
 
+--insert into padmin
 insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
 ('PADmin', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
 ('PADmin', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
@@ -21,4 +28,112 @@ insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, Pl
 ('PADmin',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
 ('PADmin', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
 ('PADmin', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL),
+('PADmin', 2495, 4000000, 99, 99, 99, 7, null, 2, null),
+('PADmin', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL),
+('PADmin', 2495, 4000000, 99, 99, 99, 7, null, 2, null),
+('PADmin', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL),
+('PADmin', 2495, 4000000, 99, 99, 99, 7, null, 2, null),
+('PADmin', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL),
 ('PADmin', 2495, 4000000, 99, 99, 99, 7, null, 2, null);
+
+--insert into padmin2
+insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
+('PADmin2', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin2', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin2', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin2', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin2', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin2', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin2',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin2',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin2',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin2',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin2',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin2',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin2', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin2', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL);
+
+--insert into padmin3
+insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
+('PADmin3', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin3', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin3', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin3', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin3', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin3', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin3',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin3',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin3',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin3',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin3',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin3',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin3', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin3', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL);
+
+--insert into padmin4
+insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
+('PADmin4', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin4', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin4', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin4', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin4', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin4', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin4',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin4',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin4',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin4',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin4',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin4',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin4', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin4', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL);
+
+--team inserts
+insert into Team(Email, TeamName, LeaderMonster, SubMonsterOne, SubMonsterTwo, SubMonsterThree, SubMonsterFour, AwokenBadgeName) values
+--insert into padmin
+('padmin', 'PADmin',100000005, 100000006, 100000007, 100000008, 100000009, NULL),
+('padmin', 'PADmin 2nd team', 100000004, NULL, NULL, NULL, NULL, 'Team Cost +100'),
+--insert into padmin2
+('padmin2', 'PADmin2', 100000005, NULL, NULL, NULL, NULL, NULL),
+--insert into padmin3
+('padmin3',	'PADmin3', 100000006, NULL, NULL, NULL, NULL, 'Team Cost +100'),
+--insert into padmin4
+('padmin4', 'PADmin4', 100000007, NULL, NULL, NULL,	NULL, NULL);
