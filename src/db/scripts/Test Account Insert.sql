7c4962abcd84931973de23e7bb1e7a673a891ed0
Elie Godbout <elie.godbout@mckenzie.edu>
2017-08-01 10:47:26 -0300

    August 1st 2017 - updated the inserts for test accounts.

--- src/db/scripts/Test Account Insert.sql
+++ src/db/scripts/Test Account Insert.sql
@@ -127,14 +127,151 @@ insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, Pl
 ('PADmin@PAD.min4', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
 ('PADmin@PAD.min4', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL);
 
+--insert into PADmin@PAD.min5
+insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
+('PADmin@PAD.min5', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin@PAD.min5', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin@PAD.min5', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin@PAD.min5', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin@PAD.min5', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin@PAD.min5', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin@PAD.min5',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin@PAD.min5',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin@PAD.min5',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin@PAD.min5',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin@PAD.min5',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin@PAD.min5',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin@PAD.min5', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin@PAD.min5', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL);
+
+--insert into PADmin@PAD.min6
+insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
+('PADmin@PAD.min6', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin@PAD.min6', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin@PAD.min6', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin@PAD.min6', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin@PAD.min6', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin@PAD.min6', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin@PAD.min6',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin@PAD.min6',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin@PAD.min6',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin@PAD.min6',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin@PAD.min6',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin@PAD.min6',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin@PAD.min6', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin@PAD.min6', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL);
+
+--insert into PADmin@PAD.min7
+insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
+('PADmin@PAD.min7', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin@PAD.min7', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin@PAD.min7', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin@PAD.min7', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin@PAD.min7', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin@PAD.min7', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin@PAD.min7',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin@PAD.min7',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin@PAD.min7',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin@PAD.min7',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin@PAD.min7',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin@PAD.min7',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin@PAD.min7', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin@PAD.min7', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL);
+
+--insert into PADmin@PAD.min8
+insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
+('PADmin@PAD.min8', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin@PAD.min8', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin@PAD.min8', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin@PAD.min8', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin@PAD.min8', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin@PAD.min8', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin@PAD.min8',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin@PAD.min8',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin@PAD.min8',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin@PAD.min8',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin@PAD.min8',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin@PAD.min8',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin@PAD.min8', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin@PAD.min8', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL);
+
+--insert into PADmin@PAD.min9
+insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
+('PADmin@PAD.min9', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin@PAD.min9', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin@PAD.min9', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin@PAD.min9', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin@PAD.min9', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin@PAD.min9', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin@PAD.min9',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin@PAD.min9',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin@PAD.min9',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin@PAD.min9',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin@PAD.min9',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin@PAD.min9',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin@PAD.min9', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin@PAD.min9', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL);
+
+--insert into PADmin@PAD.min10
+insert into MonsterInstance(Email, MonsterClassID, CurrentExperience, PlusHP, PlusATK, PlusRCV, SkillsAwoke, AssistMonsterID, SkillLevel, LSListID) Values
+('PADmin@PAD.min10', 3070, 4000000, 1, 0, 44, 9, null, 1, null),
+('PADmin@PAD.min10', 2567, 5000000, 99, 99, 99, 9, null, 1, null),
+('PADmin@PAD.min10', 2995, 5000000, 12, 12, 32, 5, null, 1, null),
+('PADmin@PAD.min10', 1919, 1500000, 2, 0, 0, 5, NULL, 2, NULL),
+('PADmin@PAD.min10', 1758, 2940630, 12, 12, 12, 4, NULL, 2, NULL),
+('PADmin@PAD.min10', 920, 3000000, 0, 0, 50, 4, NULL, 1, NULL),
+('PADmin@PAD.min10',2592,5000000,51,68,52,9,Null,1,Null),
+('PADmin@PAD.min10',3284,4537584,99,35,84,1,Null,1,Null),
+('PADmin@PAD.min10',3581,5000000,3,53,3,8,Null,1,Null),
+('PADmin@PAD.min10',	2441,	5000000,	85,	87,	82,	5,	NULL,	1,	NULL),
+('PADmin@PAD.min10',	1756,	4000000,	3,	5,	3,	7,	NULL,	1,	NULL),
+('PADmin@PAD.min10',	2496,	4000000,	4,	5,	5,	7,	NULL,	2,	NULL),
+('PADmin@PAD.min10', 2968, 2166960, 6, 13, 6, 2, NULL, 1, NULL),
+('PADmin@PAD.min10', 3277, 2346643, 59, 10, 13, 6, NULL, 2, NULL);
+
 --team inserts
 insert into Team(Email, TeamName, LeaderMonster, SubMonsterOne, SubMonsterTwo, SubMonsterThree, SubMonsterFour, AwokenBadgeName) values
 --insert into PADmin@PAD.min
-('PADmin@PAD.min', 'Team 1',100000005, 100000006, 100000007, 100000008, 100000009, NULL),
-('PADmin@PAD.min', 'Team 2', 100000004, NULL, NULL, NULL, NULL, 'Team Cost +100'),
+('PADmin@PAD.min', 'Test1 Team 1',100000005, 100000006, 100000007, 100000008, 100000009, NULL),
+('PADmin@PAD.min', 'Test1 Team 2', 100000004, NULL, NULL, NULL, NULL, 'Team Cost +100'),
 --insert into PADmin@PAD.min2
-('PADmin@PAD.min2', 'Team 1', 100000065, NULL, NULL, NULL, NULL, NULL),
+('PADmin@PAD.min2', 'Test2 Team 1', 100000065, NULL, NULL, NULL, NULL, NULL),
 --insert into PADmin@PAD.min3
-('PADmin@PAD.min3',	'Team 1', 100000080, NULL, NULL, NULL, NULL, 'Team Cost +100'),
+('PADmin@PAD.min3',	'Test3 Team 1', 100000080, NULL, NULL, NULL, NULL, 'Team Cost +100'),
 --insert into PADmin@PAD.min4
-('PADmin@PAD.min4', 'Team 1', 100000095, NULL, NULL, NULL,	NULL, NULL);
+('PADmin@PAD.min4', 'Test4 Team 1', 100000091, NULL, NULL, NULL,	NULL, NULL),
+('PADmin@PAD.min4', 'Test4 Team 2', 100000088, NULL, NULL, NULL,	NULL, NULL),
+('PADmin@PAD.min4', 'Test4 Team 3', 100000093, NULL, NULL, NULL,	NULL, NULL),
+('PADmin@PAD.min4', 'Test4 Team 4', 100000094, NULL, NULL, NULL,	NULL, NULL),
+('PADmin@PAD.min4', 'Test4 Team 5', 100000100, NULL, NULL, NULL,	NULL, NULL),
+--insert into PADmin@PAD.min5
+('PADmin@PAD.min5', 'Test5 Team 1', 100000105, NULL, NULL, NULL,	NULL, NULL),
+('PADmin@PAD.min5', 'Test5 Team 2', 100000102, NULL, NULL, NULL,	NULL, NULL),
+('PADmin@PAD.min5', 'Test5 Team 3', 100000107, NULL, NULL, NULL,	NULL, NULL),
+('PADmin@PAD.min5', 'Test5 Team 4', 100000108, NULL, NULL, NULL,	NULL, NULL),
+('PADmin@PAD.min5', 'Test5 Team 5', 100000114, NULL, NULL, NULL,	NULL, NULL);
+
+--follower inserts
+insert into Follower (Email, FollowingEmail) values
+--padmin@PAD.min10 following everyone
+('padmin@pad.min10',	'PADmin@PAD.min5'),
+('padmin@pad.min10',	'PADmin@PAD.min'),
+('padmin@pad.min10',	'PADmin@PAD.min8'),
+('padmin@pad.min10',	'PADmin@PAD.min9'),
+('padmin@pad.min10',	'PADmin@PAD.min6'),
+('padmin@pad.min10',	'PADification@gmail.com'),
+('padmin@pad.min10',	'PADmin@PAD.min2'),
+('padmin@pad.min10',	'PADmin@PAD.min3'),
+('padmin@pad.min10',	'PADmin@PAD.min4'),
+('padmin@pad.min10',	'PADmin@PAD.min7'),
+--everyone following PADmin@PAD.min10
+('PADmin@PAD.min5',		'padmin@pad.min10'),
+('PADmin@PAD.min',		'padmin@pad.min10'),
+('PADmin@PAD.min8',		'padmin@pad.min10'),
+('PADmin@PAD.min9',		'padmin@pad.min10'),
+('PADmin@PAD.min6',		'padmin@pad.min10'),
+('PADification@gmail.com', 'padmin@pad.min10'),
+('PADmin@PAD.min2',		'padmin@pad.min10'),
+('PADmin@PAD.min3',		'padmin@pad.min10'),
+('PADmin@PAD.min4',		'padmin@pad.min10'),
+('PADmin@PAD.min7',		'padmin@pad.min10');
